6bd33cabfbd043e8d41558719d0cf7bec9e9e0a1
Yasuchika Onuki <yasuchikaonuki@COAMAC14100265.local>
2018-05-01 18:54:02 +0900

    first commit

---
+++ bot.rb
@@ -0,0 +1,42 @@
+# coding: utf-8
+require 'twitter'
+
+# なんかクライアント系は全部グローバルの設定で問題ないでしょう
+# こっちでツイートします
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key =        "CONSUMER_KEY"
+  config.consumer_secret =     "CONSUMER_SECRET"
+  config.access_token =        "ACCESS_TOKEN"
+  config.access_token_secret = "ACCESS_TOKEN_SECRET"
+end
+
+# こっちでTLを読み込みます
+$stream_client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key =        "CONSUMER_KEY"
+  config.consumer_secret =     "CONSUMER_SECRET"
+  config.access_token =        "ACCESS_TOKEN"
+  config.access_token_secret = "ACCESS_TOKEN_SECRET"
+end
+
+# tweetのテストコードです
+#client.update("テスト")
+
+# Tweetへのリプライに使います
+def reply(tweet)
+  $client.update("@#{tweet.user.screen_name}\nおめでとうございます！", option = {:in_reply_to_status_id => tweet.id})
+  $client.favorite(tweet.id)
+end
+
+# TL読み込みのテストコードです
+$stream_client.user do |tweet|
+  if tweet.is_a?(Twitter::Tweet)
+    puts(tweet.user.name)
+    puts("@#{tweet.user.screen_name}")
+    puts(tweet.text)
+    puts("----------")
+    # これだとreplyをもらったときにも引っかかっちゃうので、正規表現部分を何とかします
+    if /それな$/ =~ tweet.text
+      reply(tweet)
+    end
+  end
+end
